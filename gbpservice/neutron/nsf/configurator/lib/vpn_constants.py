81e88cadd91484c5337274902c0c8872a0365f85
Suresh <suresh.dharavath@oneconvergence.com>
2016-02-24 17:33:09 +0530

    modified vpn constants

--- gbpservice/neutron/nsf/configurator/lib/vpn_constants.py
+++ gbpservice/neutron/nsf/configurator/lib/vpn_constants.py
@@ -1,17 +1,3 @@
-# One Convergence, Inc. CONFIDENTIAL
-# Copyright (c) 2012-2016, One Convergence, Inc., USA
-# All Rights Reserved.
-#
-# All information contained herein is, and remains the property of
-# One Convergence, Inc. and its suppliers, if any. The intellectual and
-# technical concepts contained herein are proprietary to One Convergence,
-# Inc. and its suppliers.
-#
-# Dissemination of this information or reproduction of this material is
-# strictly forbidden unless prior written permission is obtained from
-# One Convergence, Inc., USA
-
-
 DRIVERS_DIR = '/usr/lib/python2.7/dist-packages/gbpservice/neutron/nsf/'\
               'configurator/drivers/vpn'
 
@@ -21,6 +7,7 @@ STATE_PENDING = 'PENDING_CREATE'
 STATE_INIT = 'INIT'
 STATE_ACTIVE = 'ACTIVE'
 STATE_ERROR = 'ERROR'
+NEUTRON = 'NEUTRON'
 
 CONFIGURATION_SERVER_PORT = 8888
 request_url = "http://%s:%s/%s"
@@ -34,3 +21,4 @@ VPN_GENERIC_CONFIG_RPC_TOPIC = "vyos_vpn_topic"
 
 VPN_PLUGIN_TOPIC = 'vpn_plugin'
 VPN_AGENT_TOPIC = 'vpn_agent'
+
