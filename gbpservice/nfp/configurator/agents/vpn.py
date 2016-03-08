129be83c4d79732accbfcdd09c654f1fc1d7e190
Suresh <suresh.dharavath@oneconvergence.com>
2016-03-08 18:54:29 +0530

    changed the filter lib importing

--- gbpservice/nfp/configurator/agents/vpn.py
+++ gbpservice/nfp/configurator/agents/vpn.py
@@ -4,7 +4,7 @@ import oslo_messaging as messaging
 import requests
 
 from gbpservice.nfp.configurator.agents import agent_base
-from gbpservice.nfp.configurator.lib import filters_lib
+from gbpservice.nfp.configurator.lib import filter as filters_lib
 from gbpservice.nfp.configurator.lib import utils
 from gbpservice.nfp.configurator.lib import vpn_constants as const
 from gbpservice.nfp.core import main
