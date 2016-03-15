daa4a8cb5eac6f3b1c11af852fc4b50355511ce8
“Dhuldev-Valekar” <dhuldev.valekar@oneconvergence.com>
2016-03-15 21:14:03 +0530

    Added NFP Automation Scripts

---
+++ gbpservice/nfp/Automation-Scripts/NFP_SCRIPTS/nfp_gbp_params.sh
@@ -0,0 +1,20 @@
+# Edit following external network details (data network)
+
+inet_subnet=192.168.102.0/24
+
+inet_nat_pool=192.168.102.0/24
+
+inet_external_segment_name="default"
+
+subnet_id="be56c7ef-5c76-4693-9b8c-b2afb4fe1cd7"
+
+# Edit following external network details (service management)
+
+SM_subnet=10.41.41.0/24
+
+SM_nat_pool=12.10.0.0/24
+
+SM_external_segment_name="service-mgmt"
+
+sm_subnet_id="service-mgmt-subnet-id"
+
