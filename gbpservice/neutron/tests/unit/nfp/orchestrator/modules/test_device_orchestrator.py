a6babaa95a0a110976068eb110a29b149c461747
Jiahao Liang <gzliangjiahao@gmail.com>
2016-05-19 18:30:19 -0700

    Merge branch 'non-hotplug-support-966' into lbaasv2-dev-mitaka

--- gbpservice/neutron/tests/unit/nfp/orchestrator/modules/test_device_orchestrator.py
+++ gbpservice/neutron/tests/unit/nfp/orchestrator/modules/test_device_orchestrator.py
@@ -48,6 +48,7 @@ class DummyEvent(object):
         self.data['interfaces_in_use'] = 1
         self.data['reference_count'] = ref_count
         self.data['service_details'] = {'service_vendor': 'vyos'}
+        self.context = {}
 
 
 class HaproxyDummyDriver(object):
@@ -65,7 +66,6 @@ cfg.CONF.import_group('keystone_authtoken', 'keystonemiddleware.auth_token')
 orchestration_driver = HaproxyDummyDriver()
 NDO_CLASS_PATH = ('gbpservice.nfp.orchestrator'
                   '.modules.device_orchestrator')
-ORCHESTRATOR_LIB_PATH = ('gbpservice.nfp.orchestrator.lib')
 
 
 class NDOModuleTestCase(unittest.TestCase):
@@ -197,8 +197,6 @@ class NDORpcApiTestCase(unittest.TestCase):
        mock.MagicMock(return_value=orchestration_driver))
 @patch(NDO_CLASS_PATH + '.NDOConfiguratorRpcApi.__init__',
        mock.MagicMock(return_value=None))
-@patch(ORCHESTRATOR_LIB_PATH + '.extension_manager.ExtensionManager',
-       mock.MagicMock(return_value=DummyExtensionManager()))
 class DeviceOrchestratorTestCase(unittest.TestCase):
 
     def _initialize_ndo_handler(self):
@@ -282,7 +280,7 @@ class DeviceOrchestratorTestCase(unittest.TestCase):
         orig_event_data['status_description'] = ''
 
         orchestration_driver.plug_network_function_device_interfaces = (
-            mock.MagicMock(return_value=True))
+            mock.MagicMock(return_value=(True, [])))
         ndo_handler._create_event = mock.MagicMock(return_value=True)
 
         orig_event_data['interfaces_in_use'] += len(orig_event_data['ports'])
@@ -293,7 +291,7 @@ class DeviceOrchestratorTestCase(unittest.TestCase):
                                            orig_event_data)
 
         orchestration_driver.plug_network_function_device_interfaces = (
-            mock.MagicMock(return_value=False))
+            mock.MagicMock(return_value=(False, [])))
         ndo_handler._create_event = mock.MagicMock(return_value=True)
         event_id = 'DEVICE_CONFIGURATION_FAILED'
 
@@ -401,7 +399,7 @@ class DeviceOrchestratorTestCase(unittest.TestCase):
         orig_event_data['status_description'] = (
             ndo_handler.status_map['ACTIVE'])
         orchestration_driver.unplug_network_function_device_interfaces = (
-            mock.MagicMock(return_value=True))
+            mock.MagicMock(return_value=(True, [])))
 
         ndo_handler.unplug_interfaces(self.event)
 
@@ -417,7 +415,7 @@ class DeviceOrchestratorTestCase(unittest.TestCase):
             ndo_handler.status_map['ACTIVE'])
 
         orchestration_driver.unplug_network_function_device_interfaces = (
-            mock.MagicMock(return_value=False))
+            mock.MagicMock(return_value=(False, [])))
 
         ndo_handler.unplug_interfaces(self.event)
         mock_update_nsd.assert_called_with(ndo_handler.db_session,
